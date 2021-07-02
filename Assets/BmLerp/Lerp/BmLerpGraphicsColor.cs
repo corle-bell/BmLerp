6edd7da441922b38abeccd9d0eb82c343fea0850
Badman <543538258@qq.com>
2021-07-02 09:53:43 +0800

    Update BmLerpGraphicsColor.cs
    修改默认颜色值为白色

--- Assets/BmLerp/Lerp/BmLerpGraphicsColor.cs
+++ Assets/BmLerp/Lerp/BmLerpGraphicsColor.cs
@@ -7,8 +7,8 @@ namespace Bm.Lerp
 {
     public class BmLerpGraphicsColor : BmLerpBase
     {
-        public Color start;
-        public Color end;
+        public Color start=Color.white;
+        public Color end=Color.white;
 
         public Graphic graphic;
         protected override void _Lerp(float _per)
